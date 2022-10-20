2dfcf3d616e0f04709e97cc6ee25eb84355e5c0a
Tarun Verma <tarunverma@Taruns-MacBook-Air.local>
2022-10-20 22:34:14 +0530

    ititial cmmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import { uploadAudio } from './api';
 
 function App() {
+
+  const onLoad = (res) => {
+
+    console.log("in Onload")
+    console.log("response -> ", res)
+  }
+  
+  var getBase64 = (file) => {
+
+    return new Promise(resolve => {
+
+      let fileInfo;
+      let baseURL = "";
+      // Make new FileReader
+      let reader = new FileReader();
+      
+      // Convert the file to base64 text
+      reader.readAsDataURL(file);
+      
+      // on reader load somthing...
+      reader.onload = () => {
+        // Make a fileInfo Object
+        console.log("Called", reader);
+        baseURL = reader.result;
+        // console.log(baseURL);
+        resolve(baseURL);
+      };
+      console.log(fileInfo);
+      
+    });
+  };
+  const [file, setFile] = useState();
+  const [base64URL, setBase64URL] = useState();
+  
+  var handleFileInputChange = e => {
+    console.log(e.target.files[0]);
+
+    
+    setFile(e.target.files[0]);
+
+    getBase64(file)
+      .then(result => {
+        // console.log(result)
+       
+        setBase64URL(result)
+      })  
+      .catch(err => {
+        console.log(err);
+      });
+
+    setFile(e.target.files[0])
+  };
+
+
+  const onClick = () => {
+    uploadAudio({
+      "audio": base64URL
+    }, onLoad)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <div>
+        <input type="file" name="file" onChange={handleFileInputChange} />
+
+        <button onClick={onClick}>Submit</button>
+      </div>
+      </main>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://competishun-api.moneyyapp.in/v1/upload/audioUpload?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyUGhvbmVOdW1iZXIiOiI4NjE5ODgzNjYzIiwiaWF0IjoxNjQ3MjYwMDY0fQ.fOYX04YwCJCbH1Zs3V5OMV6BaXAzyYhK7uVDANUf_Ds",
+});
+
+export const uploadAudio = (data, onLoad) => {
+
+    console.log("inside upload")
+
+    try {
+        var config = {
+            method: 'post',
+            url: 'https://competishun-api.moneyyapp.in/v1/upload/audioUpload?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyUGhvbmVOdW1iZXIiOiI4NjE5ODgzNjYzIiwiaWF0IjoxNjQ3MjYwMDY0fQ.fOYX04YwCJCbH1Zs3V5OMV6BaXAzyYhK7uVDANUf_Ds',
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            data : data
+        };
+        
+        
+        axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+
+            
+            onLoad(response.data)
+
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+    } catch (err) {
+        console.log(err)
+    }
+    
+    
+}
